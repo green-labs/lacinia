2b65f6a8aab71f660110e551a481eb77ea595d1d
Gyeongmin Park <ddogi1513@naver.com>
2024-07-19 14:46:35 +0900

    refactor

--- src/com/walmartlabs/lacinia/complexity_analysis.clj
+++ src/com/walmartlabs/lacinia/complexity_analysis.clj
@@ -1,14 +1,25 @@
 (ns com.walmartlabs.lacinia.complexity-analysis
-  (:require [clojure.core.match :refer [match]]))
+  (:require [clojure.core.match :refer [match]]
+            [com.walmartlabs.lacinia.internal-utils :refer [cond-let]]
+            [com.walmartlabs.lacinia.selection :as selection]))
 
 (defn- parse-query
-  "- leaf node -> nil
+  "- leaf field -> nil
    - field & selection -> arguments 있으면 추가로 파싱해서 세팅 후 selections 재귀 호출
    - inline fragment -> selections를 재귀 호출
    - named fragment -> fragment-name으로 fragment-map 조회 후 해당 fragment의 selections를 재귀 호출"
   [{:keys [arguments fragment-name selections field-name leaf?]
-    :as   _selection} fragment-map]
-  (match [leaf? fragment-name]
+    :as   selection} fragment-map]
+  (cond
+   leaf? nil
+   (= :named-fragment (selection/selection-kind selection)) (let [{fragment-selections :selections} (fragment-name fragment-map)]
+                                                              (mapcat #(parse-query % fragment-map) fragment-selections))
+   (= :inline-fragment (selection/selection-kind selection)) (mapcat #(parse-query % fragment-map) selections)
+   :else (cond-> {:field-name field-name}
+           selections (assoc :selections (mapcat #(parse-query % fragment-map) selections))
+           arguments (assoc :arguments arguments)
+           true vector))
+  #_(match [leaf? fragment-name]
     ;; for leaf field
     [true _] nil
     [false nil] (cond-> {:field-name field-name}
@@ -19,7 +30,7 @@
     [nil nil] (mapcat #(parse-query % fragment-map) selections)
     ;; for named fragment
     [nil _] (let [{fragment-selections :selections} (fragment-name fragment-map)]
-                (mapcat #(parse-query % fragment-map) fragment-selections))))
+              (mapcat #(parse-query % fragment-map) fragment-selections))))
 
 (defn- count-nodes
   "- pageInfo -> 0 반환
@@ -28,7 +39,9 @@
    - connection o -> connection은 resource가 아니므로 연산에서 제외되어야 합니다.
                   -> selections에 대해 재귀호출 후 합연산 후 n-nodes 만큼 곱해줍니다.
    - connection x -> selections에 대해 재귀호출 후 합연산 후 n-nodes 만큼 곱해준 결과에 n-nodes를 더해줍니다"
-  [{:keys [field-name selections arguments]}]
+  [{:keys [field-name selections arguments]}] 
+  #_(cond-let
+   (and (= field-name :pageInfo) ))
   (let [{:keys [first last limit]} arguments
         n-nodes                    (or first last limit 1)
         leaf-node                  (seq selections)
@@ -53,9 +66,9 @@
 
 (defn complexity-analysis
   [query {:keys [max-complexity] :as _options}]
-  (when max-complexity
-   (let [{:keys [fragments selections]} query
+  (let [{:keys [fragments selections]} query
         pq (mapcat #(parse-query % fragments) selections)
         complexity (count-nodes pq)]
+    (prn complexity pq)
     (when (> complexity max-complexity)
-      {:message (format "Over max complexity! Current number of resources to be queried: %s" complexity)}))))
+      {:message (format "Over max complexity! Current number of resources to be queried: %s" complexity)})))

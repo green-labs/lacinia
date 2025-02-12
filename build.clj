9c5563cf759212edac884edc1f02750caf08bb17
Gyeongmin Park <ddogi1513@naver.com>
2025-02-12 17:23:41 +0900

    fix

--- build.clj
+++ build.clj
@@ -15,118 +15,74 @@
 ;; clj -T:build <var>
 
 (ns build
-  (:require [clojure.string :as string]
-            [clojure.tools.build.api :as build]
+  (:require [clojure.tools.build.api :as build]
             [deps-deploy.deps-deploy :as dd]
-            #_[net.lewisship.build :as b]))
-
-;; (def lib 'org.clojars.greenlabs/lacinia)
-;; (def version (-> "VERSION.txt" slurp string/trim))
-;; (def class-dir "target/classes")
-
-;; (def jar-params {:project-name lib
-;;                  :version version
-;;                  :class-dir class-dir})
-
-;; (defn clean
-;;   [_params]
-;;   (build/delete {:path "target"}))
-
-;; (defn compile-java [_]
-;;   (build/javac {:src-dirs ["java"]
-;;                 :class-dir class-dir
-;;                 :basis (build/create-basis)
-;;                 :javac-opts ["--release" "11"]}))
-
-;; (defn jar
-;;   [_params]
-;;   (compile-java nil)
-;;   (b/create-jar jar-params))
-
-;; (defn deploy
-;;   [_params]
-;;   (clean nil)
-;;   (b/deploy-jar (jar nil)))
-
-;; (defn codox
-;;   [_params]
-;;   (b/generate-codox {:project-name lib
-;;                      :version version
-;;                      :aliases [:dev]}))
-
-;; (def publish-dir "../apidocs/lacinia")
-
-;; (defn publish
-;;   "Generate Codox documentation and publish via a GitHub push."
-;;   [_params]
-;;   (println "Generating Codox documentation")
-;;   (codox nil)
-;;   (println "Copying documentation to" publish-dir "...")
-;;   (build/copy-dir {:target-dir publish-dir
-;;                    :src-dirs ["target/doc"]})
-;;   (println "Committing changes ...")
-;;   (build/process {:dir publish-dir
-;;                   :command-args ["git" "commit" "-a" "-m" (str "lacinia " version)]})
-;;   (println "Pushing changes ...")
-;;   (build/process {:dir publish-dir
-;;                   :command-args ["git" "push"]}))
+            [net.lewisship.build :as b]))
 
 (def lib 'org.clojars.greenlabs/lacinia)
 (def version (format "1.0.%s" (build/git-count-revs nil)))
 (def class-dir "target/classes")
+(def basis (build/create-basis {:project "deps.edn"}))
 
-(def ^:private basis
-  (delay (build/create-basis {})))
-
-(defn- pom-template
-  [version]
-  [[:description "A fork of lacinia developed and maintained by Greenlabs "]
-   [:url "https://github.com/green-labs/lacinia"]
-   [:licenses
-    [:license
-     [:name "Apache License, Version 2.0"]
-     [:url "http://www.apache.org/licenses/LICENSE-2.0"]]]
-   [:scm
-    [:url "https://github.com/green-labs/lacinia"]
-    [:connection "scm:git:https://github.com/green-labs/lacinia.git"]
-    [:developerConnection "scm:git:ssh:git@github.com:green-labs/lacinia.git"]
-    [:tag (str "v" version)]]])
-
-(defn- jar-opts
-  [opts]
-  (println "Version:" version)
-  (assoc opts
-         :lib lib   :version version
-         :jar-file  (format "target/%s-%s.jar" lib version)
-         :basis     @basis
-         :class-dir class-dir
-         :target    "target"
-         :src-dirs  ["src"]
-         :pom-data  (pom-template version)))
-
-(defn clean
-  [_]
-  (println "Removing target/ ...")
+(defn clean [_]
+  (println "Cleaning target directory...")
   (build/delete {:path "target"}))
 
-(defn build-jar
-  [opts]
-  (let [j-opts (jar-opts opts)]
-    (println "Writing pom.xml ...")
-    (build/write-pom j-opts)
-    (println "Copying source ...")
-    (build/copy-dir {:src-dirs ["src"] :target-dir class-dir})
-    (println "Building" (:jar-file j-opts) "...")
-    (build/jar j-opts)))
+(defn compile-java [_]
+  (println "Compiling Java sources...")
+  (build/javac {:src-dirs ["java"]
+                :class-dir class-dir
+                :basis basis
+                :javac-opts ["--release" "11"]}))
 
-(defn deploy
-  "Build the jar and deploy it to Clojars. Expects env vars CLOJARS_USERNAME &
-  CLOJARS_PASSWORD."
-  [opts]
-  (clean opts)
-  (build-jar opts)
-  (let [{:keys [jar-file] :as opts} (jar-opts opts)]
-    (dd/deploy {:installer :remote
-                :artifact  (build/resolve-path jar-file)
-                :pom-file  (build/pom-path (select-keys opts [:lib :class-dir]))}))
-  opts)
+(defn jar [_]
+  (println "Building jar...")
+  (build/write-pom {:class-dir class-dir
+                    :lib lib
+                    :version version
+                    :basis basis
+                    :src-dirs ["src"]
+                    :scm {:url "https://github.com/green-labs/lacinia"
+                          :connection "scm:git:https://github.com/green-labs/lacinia.git"
+                          :tag (str "v" version)}
+                    :pom-data [[:licenses
+                                [:license
+                                 [:name "Apache License, Version 2.0"]
+                                 [:url "http://www.apache.org/licenses/LICENSE-2.0"]
+                                 [:distribution "repo"]]]]
+                    :description "A fork of lacinia developed and maintained by Greenlabs"})
+  (build/jar {:class-dir class-dir
+              :jar-file (format "target/%s-%s.jar" lib version)}))
+
+(defn deploy [_]
+  (println "Starting deployment process...")
+  (clean nil)
+  ;; prep-lib이 자동으로 compile-java를 실행합니다
+  (jar nil)
+  (println "Deploying to Clojars...")
+  (dd/deploy {:installer :remote
+              :artifact (format "target/%s-%s.jar" lib version)
+              :pom-file (str class-dir "/META-INF/maven/" lib "/pom.xml")}))
+
+(defn codox
+  [_params]
+  (b/generate-codox {:project-name lib
+                     :version version
+                     :aliases [:dev]}))
+
+(def publish-dir "../apidocs/lacinia")
+
+(defn publish
+  "Generate Codox documentation and publish via a GitHub push."
+  [_params]
+  (println "Generating Codox documentation")
+  (codox nil)
+  (println "Copying documentation to" publish-dir "...")
+  (build/copy-dir {:target-dir publish-dir
+                   :src-dirs ["target/doc"]})
+  (println "Committing changes ...")
+  (build/process {:dir publish-dir
+                  :command-args ["git" "commit" "-a" "-m" (str "lacinia " version)]})
+  (println "Pushing changes ...")
+  (build/process {:dir publish-dir
+                  :command-args ["git" "push"]}))

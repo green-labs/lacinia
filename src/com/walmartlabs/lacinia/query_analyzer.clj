d2ccd1053cac50abbb44b8026b259d3204b4dd4f
Gyeongmin Park <42409229+1e16miin@users.noreply.github.com>
2025-02-17 12:00:20 +0900

    쿼리 분석기 리팩토링 (#22)
    * refactor
    
    * nit

--- src/com/walmartlabs/lacinia/query_analyzer.clj
+++ src/com/walmartlabs/lacinia/query_analyzer.clj
@@ -7,13 +7,12 @@
 
 (declare summarize-selection)
 
-(defn ^:private summarize-sub-selections
+(defn ^:private summarize-partial-selections
   "여러 하위 selection들을 요약하는 함수입니다.
    
    파라미터:
    - fragment-map: 프래그먼트 정의들의 맵
-   - depth: 현재 selection의 깊이 레벨
-   - sub-selections: 처리할 하위 selection들의 시퀀스
+   - partial-selections: 처리할 하위 selection들의 시퀀스
    
    동작:
    1. 각 하위 selection에 현재 깊이 정보를 추가
@@ -22,9 +21,8 @@
 
    반환값:
    - 모든 하위 selection들의 요약 정보가 포함된 시퀀스"
-  [fragment-map depth sub-selections]
-  (let [sub-selections' (map #(assoc % :depth depth) sub-selections)]
-    (mapcat #(summarize-selection % fragment-map) sub-selections')))
+  [fragment-map partial-selections]
+  (mapcat #(summarize-selection % fragment-map) partial-selections))
 
 (defn ^:private summarize-selection
   "selection을 재귀적으로 요약하며, 필드, inline fragment, named fragment를 처리합니다.
@@ -34,10 +32,10 @@
    - fragment-map: fragment 정의들의 맵
    
    반환값:
-   - 필드인 경우: 필드 이름, depth, selection 정보를 포함하는 맵의 벡터
+   - 필드인 경우: 필드 이름, selection 정보를 포함하는 맵의 벡터
    - 프래그먼트인 경우: 프래그먼트 내부 selection들의 요약 정보
    - leaf나 pageInfo인 경우: nil"
-  [{:keys [arguments selections field-name leaf? fragment-name depth] :as selection} fragment-map]
+  [{:keys [arguments selections field-name leaf? fragment-name] :as selection} fragment-map]
   (let [selection-kind (selection/selection-kind selection)]
     (cond
       ;; If it's a leaf or `pageInfo`, return nil.
@@ -46,46 +44,42 @@
 
       ;; If it's a named fragment, look it up in the fragment-map and process its selections.
       (= :named-fragment selection-kind)
-      (let [sub-selections (:selections (fragment-map fragment-name))]
-        (summarize-sub-selections fragment-map depth sub-selections))
+      (let [partial-selections (:selections (fragment-map fragment-name))]
+        (summarize-partial-selections fragment-map partial-selections))
 
       ;; If it's an inline fragment or  `edges` field, process its selections.
       (or (= :inline-fragment selection-kind) (= field-name :edges))
-      (summarize-sub-selections fragment-map depth selections)
+      (summarize-partial-selections fragment-map selections)
 
       ;; Otherwise, handle a regular field with potential nested selections.
       :else
-      (let [depth' (inc depth)
-            n-nodes (or (-> arguments (select-keys [:first :last]) vals first) 1)]
+      (let [n-nodes (or (-> arguments (select-keys [:first :last]) vals first) 1)]
         [{:field-name field-name
-          :selections (summarize-sub-selections fragment-map depth' selections)
+          :selections (summarize-partial-selections fragment-map selections)
           :list-args? (list-args? arguments)
-          :depth depth'
           :n-nodes n-nodes}]))))
 
 (defn ^:private calculate-complexity
-  [{:keys [selections list-args? n-nodes]}]
+  "selection 요약 정보에서 complexity를 계산합니다."
+  [{:keys [selections list-args? n-nodes] :or {n-nodes 0}}]
   (let [children-complexity (apply + (map calculate-complexity selections))]
     (if list-args?
       (* n-nodes children-complexity)
       (+ n-nodes children-complexity))))
 
-(defn ^:private get-max-depth
+(defn ^:private calculate-max-depth
   "selection 요약 정보에서 최대 depth를 계산합니다."
-  [{:keys [selections depth] :or {depth 0}}]
+  [{:keys [selections]} current-depth]
   (if (empty? selections)
-    depth
-    (max depth (apply max (map get-max-depth selections)))))
+    current-depth
+    (apply max (map #(calculate-max-depth % (inc current-depth)) selections))))
 
 (defn complexity-analysis
   [query]
   (let [{:keys [fragments selections]} query
-        init-depth 0
-        summarized-selections (summarize-sub-selections fragments init-depth selections)
-        complexity (apply + (map calculate-complexity summarized-selections))
-        max-depth (get-max-depth summarized-selections)]
-    {:complexity complexity
-     :max-depth max-depth}))
+        summarized-selections {:selections (summarize-partial-selections fragments selections)}]
+    {:complexity (calculate-complexity summarized-selections)
+     :max-depth (calculate-max-depth summarized-selections 0)}))
 
 (defn enable-query-analyzer
   [context]
